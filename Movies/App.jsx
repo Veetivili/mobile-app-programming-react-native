f1c660352faa22a3faf998d5fd03e42df21a2aed
hakave <veeti.hakala@jamk.fi>
2023-08-21 14:29:59 +0300

    Exercise 4 ready

---
+++ Movies/App.jsx
@@ -0,0 +1,29 @@
+// App.js
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import MovieListScreen from './MovieListScreen';
+import MovieDetailScreen from './MovieDetailScreen';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="MoviesList"
+          component={MovieListScreen}
+          options={{ title: 'MovieList' }}
+        />
+        <Stack.Screen 
+          name="MovieDetails" 
+          component={MovieDetailScreen} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
